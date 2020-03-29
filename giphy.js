f860bc04e94ffe6bac95d051bf2269b84f9bf4b8
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-03-28 18:40:05 -0700

    fix bugs, 1click render, alert error if no input, clear input after submit

--- giphy.js
+++ giphy.js
@@ -14,76 +14,73 @@ $(document).ready(function() {
       $(areaToPlace).append(a);
     }
   }
-  //Click a button to display name, rating and gifs, of the button label word/name
-  function gifFun() {
-    $(document).on("click", ".gif-btn", function() { 
-      var topic = $(this).attr("data-name");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&api_key=dc6zaTOxFJmzC&limit=10";
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-        var gifInf = response.data;
-
-        for (var i = 0; i < gifInf.length; i++) {
-          var gifDiv = $("<div>");
-          var PRating = $("<p>").text("Rating of GIF: " + gifInf[i].rating);
-          var PName = $("<p>").text("Gif entitled: " + gifInf[i].title);
-
-          var gif = $("<img>");
-          gif.addClass("ply-gif-2");
-          gif.attr("src", gifInf[i].images.fixed_height_still.url);
-          gif.attr("data-still", gifInf[i].images.fixed_height_still.url);
-          gif.attr("data-animate", gifInf[i].images.fixed_height.url);
-          gif.attr("data-state", "still");
-          
-          gifDiv.prepend(PRating, PName);
-          gifDiv.prepend(gif);
-
-          $("#hide-1").css("display","block");
-
-          $("#show-gif").prepend(gifDiv);
-        }
-
-      });
-    });
-  };
-
-//Click a gif to play it or pause it
-  $(document).on("click", ".ply-gif-2", function() {
-    var state = $(this).attr("data-state");
-    
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
-  });
-      
-  //Use the vulue from the input box to send to the list of topics and rerender the buttons 
-  $("#btn-2").on("click", function(event) { 
-    event.preventDefault();
-
-    var inputTopic = $("#input-1").val().trim();
-    function isEmpty(val){
-      if(val) {         
-        topics.push(inputTopic);
-      renderButton(topics, "gif-btn", "#topic-buttons");
-      } else {
-        alert("Please enter a topic you love.")
-        console.log("error");
-      }
-      return (val === undefined || val == null || val.length <= 0) ? true : false;
-
-    }
-  isEmpty()
-  });
   
-  //Add event listeners
-    $(document).on("click", ".gif-btn", gifFun);
-    renderButton(topics, "gif-btn", "#topic-buttons");
-  });
+
+    //Click a button to display name, rating and gifs, of the button label word/name
+        $(document).on("click", ".gif-btn", function() { 
+          var topic = $(this).attr("data-name");
+          var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+            var gifInf = response.data;
+  
+            for (var i = 0; i < gifInf.length; i++) {
+              var gifDiv = $("<div>");
+              var PRating = $("<p>").text(gifInf[i].rating);
+              var PName = $("<p>").text(gifInf[i].title);
+  
+              var gif = $("<img>");
+              gif.addClass("ply-gif-2");
+              gif.attr("src", gifInf[i].images.fixed_height_still.url);
+              gif.attr("data-still", gifInf[i].images.fixed_height_still.url);
+              gif.attr("data-animate", gifInf[i].images.fixed_height.url);
+              gif.attr("data-state", "still");
+              
+              gifDiv.prepend(PRating, PName);
+              gifDiv.prepend(gif);
+  
+              $("#hide-1").css("display","block");
+  
+              $("#show-gif").prepend(gifDiv);
+            }
+  
+          });
+        });
+
+      //Click a gif to play it or pause it
+          $(document).on("click", ".ply-gif-2", function() {
+            var state = $(this).attr("data-state");
+            
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+      
+   //Use the vulue from the input box to send to the list of topics and rerender the buttons 
+      $("#btn-2").on("click", function(event) { 
+        event.preventDefault();
+  
+        var inputTopic = $("#input-1").val().trim();
+        if(inputTopic == undefined || null || inputTopic.length == 0) {
+          alert("Enter a topic you love");
+        } else {
+        topics.push(inputTopic);
+      }
+
+        console.log(inputTopic);
+
+        $("#input-1").val("");
+  
+        renderButton(topics, "gif-btn", "#topic-buttons");
+      });
+  
+      renderButton(topics, "gif-btn", "#topic-buttons");
+    });
